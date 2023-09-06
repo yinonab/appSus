ca83dc6d6ade63a21aa40f48b8b3c3658bb294cc
shayel1212 <shayelmomo@gmail.com>
2023-09-07 00:49:23 +0300

    add mail staring functionality

--- cmps/MailPreview.jsx
+++ cmps/MailPreview.jsx
@@ -1,8 +1,10 @@
 const { useNavigate } = ReactRouterDOM;
+const { useState } = React;
 import { mailService } from "../apps/mail/services/mail-service.js";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export function MailPreview({ mail }) {
   const navigate = useNavigate();
+  const [isStarred, setIsStarred] = useState(false);
   const date = new Date(mail.sentAt);
   const formattedDate = date.toLocaleDateString("en-US", {
     day: "numeric",
@@ -14,12 +16,34 @@ export function MailPreview({ mail }) {
     console.log(mail);
     navigate(`/mail/${mail.id}`);
   }
+  function addAndRemoveStar(ev) {
+    ev.preventDefault;
+    if (mail.isStarred) {
+      mail.isStarred = false;
+      mailService.save(mail).then(() => setIsStarred(false));
+    } else {
+      mail.isStarred = true;
+      mailService.save(mail).then(() => setIsStarred(true));
+    }
+  }
   return (
     <tbody>
-      <tr onClick={onCLickMail}>
-        <td className="bold">{mail.subject}</td>
-        <td>{mail.body}</td>
-        <td className="bold">{formattedDate}</td>
+      <tr>
+        <td onClick={onCLickMail} className="bold">
+          {mail.subject}
+        </td>
+        <td onClick={onCLickMail}>{mail.body}</td>
+        <td onClick={onCLickMail} className="bold">
+          {formattedDate}
+        </td>
+        <td onClick={addAndRemoveStar}>
+          <button>
+            <i
+              style={mail.isStarred ? { color: "gold" } : { color: "black" }}
+              className="fa-solid fa-star"
+            ></i>
+          </button>
+        </td>
       </tr>
     </tbody>
   );
