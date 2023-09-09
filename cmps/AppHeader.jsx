f6ffb2f19d94de73354e217e0fff6aa0b74ceb21
yinonab <yinonabarjel@gmail.com>
2023-09-10 00:05:59 +0300

    remove logo

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,7 +7,7 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <h3>.</h3>
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
